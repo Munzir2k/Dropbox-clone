afa72bf5bc68e999423ae2520d8d0e21342b2d92
Munzir2k <mohammadmunzir2k@gmail.com>
2025-07-11 20:37:59 +0800

    Logo changed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,41 +1,40 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-import { ThemeToggler } from './ThemeToggler';
+/** @format */
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ThemeToggler } from "./ThemeToggler";
 
 function Header() {
-	return (
-		<header className='flex items-center justify-between'>
-			<Link
-				href='/'
-				className='flex items-center space-x-2'
-			>
-				<div className='bg-[#0160FE] w-fit'>
-					<Image
-						src='https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png'
-						alt='logo'
-						className='invert'
-						height={50}
-						width={50}
-					/>
-				</div>
-				<h1 className='font-bold text-xl'>Dropbox</h1>
-			</Link>
+    return (
+        <header className="flex items-center justify-between">
+            <Link href="/" className="flex items-center space-x-2">
+                <div className="bg-[#0160FE] w-fit">
+                    <Image
+                        src="https://www.shareicon.net/data/512x512/2016/07/10/119920_dropbox_512x512.png"
+                        alt="logo"
+                        className="invert"
+                        height={50}
+                        width={50}
+                    />
+                </div>
+                <h1 className="font-bold text-xl">Dropbox</h1>
+            </Link>
 
-			<div className='flex px-5 space-x-2 items-center'>
-				{/* Theme toggler */}
-				<ThemeToggler />
+            <div className="flex px-5 space-x-2 items-center">
+                {/* Theme toggler */}
+                <ThemeToggler />
 
-				<UserButton afterSignOutUrl='/' />
-				<div>
-					<SignedOut>
-						<SignInButton afterSignInUrl='/dashboard' />
-					</SignedOut>
-				</div>
-			</div>
-		</header>
-	);
+                <UserButton afterSignOutUrl="/" />
+                <div>
+                    <SignedOut>
+                        <SignInButton afterSignInUrl="/dashboard" />
+                    </SignedOut>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
